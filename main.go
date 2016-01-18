588c51654c9401eefd9c30520ad8acd23eb7a33b
Edward Knowles <mail@eknowles.com>
2016-01-18 21:35:13 +0000

    foo

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
+	"flag"
 	"net/http"
+	"log"
+	"bytes"
 )
 
 type Shot struct {
 	X int `json:"x"`
 	Y int `json:"x"`
 }
+
+var ourTurn = flag.Bool("starts", false, "Choose to start the game and you win the coin toss")
 
 const (
 	AircraftCarrier = 0
@@ -27,15 +33,43 @@ type Response struct {
 }
 
 func TakingFire(w http.ResponseWriter, r *http.Request) {
-	response := Response{
-		Hit:      true,
-		ShipSunk: BattleShip,
+	log.Print(*ourTurn)
+	if (*ourTurn) {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "Please wait your turn!")
+	} else {
+		response := Response{
+			Hit:      true,
+			ShipSunk: BattleShip,
+		}
+		j, _ := json.Marshal(response)
+		io.WriteString(w, string(j))
 	}
-	j, _ := json.Marshal(response)
-	io.WriteString(w, string(j))
+}
+
+func PewPew(w http.ResponseWriter, r *http.Request) {
+	if (!*ourTurn) {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "Please wait your turn!")
+	} else {
+		var shot Shot
+		body, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(body, &shot)
+		if (b2[shot.X][shot.Y] == Nothing) {
+			j, _ := json.Marshal(shot)
+			res, err := http.Post("localhost:8001/", "application/json", bytes.NewBuffer(j))
+		}
+		response := Response{
+			Hit:      true,
+			ShipSunk: BattleShip,
+		}
+		j, _ := json.Marshal(response)
+		io.WriteString(w, string(j))
+	}
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", TakingFire)
 	http.ListenAndServe(":8000", nil)
 }
